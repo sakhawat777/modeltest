26f82e92cc6a707dc676842b7ba54b1fc051bec2
sakhawat777 <biplobbms@gmail.com>
2024-05-13 12:37:00 +0600

    added  GenerationType.SEQUENCE

--- src/main/java/com/bcs/modeltest/model/Question.java
+++ src/main/java/com/bcs/modeltest/model/Question.java
@@ -11,7 +11,7 @@ import lombok.Data;
 
 public class Question {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private  int id;
     private  String questionTitle;
     private  String option1;
