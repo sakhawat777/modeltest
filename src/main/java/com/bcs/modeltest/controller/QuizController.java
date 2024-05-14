b53d6dfec20a518f719cd49c937a953fa4bf3a56
sakhawat777 <biplobbms@gmail.com>
2024-05-14 15:18:33 +0600

    Get Quiz API done

--- src/main/java/com/bcs/modeltest/controller/QuizController.java
+++ src/main/java/com/bcs/modeltest/controller/QuizController.java
@@ -1,10 +1,12 @@
 package com.bcs.modeltest.controller;
 
+import com.bcs.modeltest.model.QuestionWrapper;
 import com.bcs.modeltest.service.QuizService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -14,5 +16,10 @@ public class QuizController {
    @PostMapping("create")
 public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
     return  quizService.createQuiz(category, numQ, title);
+    }
+    @GetMapping("/get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestion(@PathVariable int id){
+       return  quizService.getQuizQuestion(id);
+
     }
 }
--- src/main/java/com/bcs/modeltest/service/QuizService.java
+++ src/main/java/com/bcs/modeltest/service/QuizService.java
@@ -3,13 +3,16 @@ package com.bcs.modeltest.service;
 import com.bcs.modeltest.dao.QuestionDao;
 import com.bcs.modeltest.dao.QuizDao;
 import com.bcs.modeltest.model.Question;
+import com.bcs.modeltest.model.QuestionWrapper;
 import com.bcs.modeltest.model.Quiz;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +27,16 @@ public class QuizService {
       quiz.setQuestions(questions);
       quizDao.save(quiz);
       return new ResponseEntity<>("Success", HttpStatus.CREATED);
+   }
+
+   public ResponseEntity<List<QuestionWrapper>> getQuizQuestion(int id) {
+      Optional<Quiz> quiz = quizDao.findById(id);
+      List<Question> questionsFromDB = quiz.get().getQuestions();
+      List<QuestionWrapper> questionsForUsers = new ArrayList<>();
+      for (Question q: questionsFromDB){
+         QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+         questionsForUsers.add(qw);
+      }
+      return  new ResponseEntity<>(questionsForUsers, HttpStatus.OK);
    }
 }
